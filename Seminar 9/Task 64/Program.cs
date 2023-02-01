68ba0f0075dd6f723527972e605d63aaa8b40c81
I.S.Epifanov <isepifanov@ya.ru>
2023-02-01 12:08:46 +0300

    Домашнее задание к 9 семинару

---
+++ Seminar 9/Task 64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+void NaturalNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        NaturalNumbers(m + 1, n);
+        Console.Write($"{m} ");
+    }
+}
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+
+Console.Write($"N = {n} -> ");
+NaturalNumbers(m, n);
